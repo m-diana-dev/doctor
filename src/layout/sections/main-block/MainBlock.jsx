31441c188e956991574e81ad830575186907b577
m-diana-dev <m.diana.dev@gmail.com>
2024-02-07 17:58:41 +0300

    feat: added slider in Main block

--- src/layout/sections/main-block/MainBlock.jsx
+++ src/layout/sections/main-block/MainBlock.jsx
@@ -3,10 +3,17 @@ import {Container} from "../../../components/Container.js";
 import {FlexWrapp} from "../../../components/FlexWrapp.js";
 import {Button, StyledButton} from "../../../components/button/Button.jsx";
 import MainImg from '../../../assets/images/main-block/main.jpeg'
+import MainImg2 from '../../../assets/images/main-block/main-2.jpg'
+import AliceCarousel from "react-alice-carousel";
 
 
 //Главный блок сайта
 export const MainBlock = () => {
+    //массив элеентов для слайдера
+    const sliderItems = [
+        <img key={1} src={MainImg} alt="main"/>,
+        <img key={2} src={MainImg2} alt="main"/>,
+    ]
     return (
         <StyledMainBlock>
             <Container>
@@ -21,7 +28,15 @@ export const MainBlock = () => {
                         <Button as={'a'} href='#appointment'>Записаться на прием</Button>
                     </MainBlockContent>
                     <MainBlockImg>
-                        <img src={MainImg} alt="main"/>
+                        {/*используем библиотеку alice-carousel для слайдера */}
+                        <AliceCarousel mouseTracking items={sliderItems}
+                                       autoPlay
+                                       autoPlayInterval={3000}
+                                       animationDuration={2000}
+                                       animationType="fadeout"
+                                       disableDotsControls
+                                       disableButtonsControls
+                                       infinite/>
                     </MainBlockImg>
                 </FlexWrapp>
             </Container>
@@ -55,18 +70,18 @@ const StyledMainBlock = styled.section`
     }
   }
   ${FlexWrapp} {
-    @media ${({theme})=>theme.media.mobile}{
+    @media ${({theme})=>theme.media.tablet}{
       flex-direction: column;
     }
   }
 `
 
 const MainBlockContent = styled.div`
-  max-width: 603px;
-  width: 100%;
+  width: calc(50% - 70px);
   margin-right: 70px;
-  @media ${({theme})=>theme.media.mobile}{
+  @media ${({theme})=>theme.media.tablet}{
     max-width: 100%;
+    width: 100%;
     margin-right: 0;
     margin-bottom: 30px;
   }
@@ -76,8 +91,11 @@ const MainBlockTitle = styled.h1`
   font-weight: 300;
   line-height: 1.2;
   margin-bottom: 20px;
-  @media ${({theme})=>theme.media.desktop}{
+  @media screen and (max-width: 1400px){
     font-size: 44px;
+  }
+  @media ${({theme})=>theme.media.desktop}{
+    font-size: 40px;
   }
   @media ${({theme})=>theme.media.tablet}{
     font-size: 36px;
@@ -95,26 +113,35 @@ const MainBlockText = styled.p`
   font-size: 16px;
   line-height: 26px;
   margin-bottom: 38px;
-  @media ${({theme})=>theme.media.mobile}{
+  @media ${({theme})=>theme.media.tablet}{
     margin-bottom: 20px;
   }
 `
 const MainBlockImg = styled.div`
-  max-width: 681px;
-  width: 100%;
+  width: calc(50% + 50px);
   height: 457px;
   margin-right: -130px;
   @media ${({theme})=>theme.media.desktop}{
     margin-right: 0;
   }
-  @media ${({theme})=>theme.media.mobile}{
-    max-width: 100%;
+  @media ${({theme})=>theme.media.tablet}{
+    width: 100%;
     height: auto;
+  }
+  .alice-carousel{
+    height: 100%;
+    >div{
+      height: 100%;
+    }
+  }
+  .alice-carousel__wrapper{
+    height: 100%;
   }
   img{
     width: 100%;
     height: 100%;
     object-fit: cover;
+    object-position: center center;
     border-radius: 20px;
     @media ${({theme})=>theme.media.mobileSmall}{
       border-radius: 10px;
